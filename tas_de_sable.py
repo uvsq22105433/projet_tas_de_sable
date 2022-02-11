a3f3f47301a1802782a70badd6795c08228fc609
Diana RODRIGUEZ <diana.rodriguez@ens.uvsq.fr>
2022-02-11 10:16:45 +0100

    eho

--- tas_de_sable.py
+++ tas_de_sable.py
@@ -1,7 +1,7 @@
 ##################################
 # Groupe BI 2
 # Salma BAISS un truc
-# Diana RODRIGUEZ
+# Diana RODRIGUEZ yolo
 # Awena SALIC bonsoir 
 # https://github.com/awenax/projet_tas_de_sable
 ##################################
